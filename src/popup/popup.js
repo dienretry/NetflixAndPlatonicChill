2750434190a1405fecdf1b709ea667bd078c2223
dienretry <amelal.youssef@gmail.com>
2019-05-08 23:51:27 +0200

    beautified the popup

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,22 +1,48 @@
 document.addEventListener("DOMContentLoaded", function(event){
     var wrapper = document.getElementById("wrapper");
     var createRoomButton = document.getElementById("create");
-    //var arrow = document.getElementById("arrow");
     var url = document.getElementById("url");
-    var text = document.getElementById("text");
-    //arrow.style.display = "none";
-    text.style.display = "none";
-    url.style.display = "none"
+    var label = document.getElementById("label");
+    var copyButton = document.getElementById("copy-button");
+    label.style.display = "none";
+    url.style.display = "none";
+    copyButton.style.display = "none";
     createRoomButton.addEventListener("click", function(e){
         chrome.runtime.sendMessage({data:"createroom"});
         chrome.runtime.onMessage.addListener(function(message){
-            url.innerHTML = message.url;
+            url.value = message.url;
         });
         wrapper.style.display = "none";
         createRoomButton.style.display = "none";
-        //arrow.style.display = "block";
-        text.style.display = "block";
+        label.style.display = "block";
         url.style.display = "block";
+        copyButton.style.display = "block";
     });
+    copyButton.addEventListener("click", function(e){
+        //find target element
+        var t = e.target;
+        var c = t.dataset.copytarget;
+        var inp = (c ? document.querySelector(c) : null);
+    
+        //is element selectable
+        if (inp && inp.select) {
+            //select text
+            inp.select();
+            try {
+                //copy text
+                document.execCommand('copy');
+                inp.blur();
+    
+                //copied animation
+                t.classList.add('copied');
+                setTimeout(function() {
+                    t.classList.remove('copied');
+                }, 1500);
+            }
+            catch (err) {
+                alert('please press Ctrl/Cmd+C to copy');
+            }
+    
+        }
+    })
 });
-
